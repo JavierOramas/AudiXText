df492abb35e596df2170a49019ed325742da7196
Javier Oramas <javiale2000@gmail.com>
2020-11-27 23:11:34 -0500

    commenting and controlling exceptions

--- audio-text.py
+++ audio-text.py
@@ -2,9 +2,22 @@ import speech_recognition as sr
 from pathlib import Path
 import typer
 
-def audio_to_text(file:typer.Argument(default='input.wav')):
+#this function recieves the file path, the noise span for adjusting the noise reduction algorithm and the show_alt bool to toggle show all alternatives on transcription
+def audio_to_text(file=typer.Argument(default='input.wav'), noise_span=typer.Option(default=1), show_alt=typer.Option(default=False)):
+    #create the listener
     listener = sr.Recognizer()
+    
+    #load the file
     with file as source:
+        #adjust it for the noise reduction
+        listener.adjust_for_ambient_noise(source, duration=noise_span)
+        #record it
         audio = listener.record(source)
     
-    return listener.recognize_google(audio)
+    #get the trnascription made by google (other options can be used but this is the most popular)
+    try:
+        response = listener.recognize_google(audio, show_all=show_alt)
+    except:
+        response = 'Audio could not be transcrypted'
+    
+    return response
